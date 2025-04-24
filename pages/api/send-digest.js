f6a00aca01d167cc9b0e1dc12bfa96fe9c472366
Jennifer Tsang <jennifertsang300@gmail.com>
2025-04-23 23:03:04 -0700

    Switch to Pages Router by disabling appDir

--- pages/api/send-digest.js
+++ pages/api/send-digest.js
@@ -1,5 +1,7 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "This works using the Pages Router!",
-  });
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // REMOVE appDir to use Pages Router
 }
+
+module.exports = nextConfig
