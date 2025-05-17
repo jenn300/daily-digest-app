b3f55020ba6ed3dc218bbba260bc718fd4d1e3d7
Jennifer Tsang <jennifertsang300@gmail.com>
2025-05-16 20:31:08 -0700

    Modified env check

--- pages/api/check-env.js
+++ pages/api/check-env.js
@@ -1,5 +1,9 @@
 // pages/api/check-env.js
 export default function handler(req, res) {
-    res.status(200).json({ smtpHost: process.env.BREVO_SMTP_HOST });
+    res.status(200).json({
+      BREVO_SMTP_HOST: process.env.BREVO_SMTP_HOST,
+      BREVO_SMTP_PORT: process.env.BREVO_SMTP_PORT,
+      BREVO_SMTP_USER: process.env.BREVO_SMTP_USER ? '✅ Set' : '❌ Not Set',
+      BREVO_SMTP_PASS: process.env.BREVO_SMTP_PASS ? '✅ Set' : '❌ Not Set',
+    });
   }
-  
